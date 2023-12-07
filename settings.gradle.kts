c8b6a89fdb07c35cfa5aecdbd2fc0faecde06ea4
Aditya Mishra <adimishra016@gmail.com>
2023-12-07 18:21:36 +0530

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Viva-050"
+include(":app")
+ 
